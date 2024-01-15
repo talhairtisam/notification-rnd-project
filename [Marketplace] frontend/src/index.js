e275b34f6ec00382e8e7405c29b72303d384ca41
Taimoor <taimur.ultracodes@gmail.com>
2024-01-15 15:09:10 +0500

    marketplace frontend API for signup products integrated

--- [Marketplace] frontend/src/index.js
+++ [Marketplace] frontend/src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./Signup";
+import Products from "./Products";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/products" element={<Products/>} />
+
+
+
+    </Routes>
+  </BrowserRouter>
+   
   </React.StrictMode>,
 );
